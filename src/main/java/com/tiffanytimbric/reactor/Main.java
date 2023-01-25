588cb40e7038ff7a6f9c218059758c9afcd9787c
Tiffany Timbric <tiffanytimbric@gmail.com>
2023-01-25 05:12:19 -0700

    Added Javadoc describing the Data Flow Types supported by the Reactive eXtension API.

--- src/main/java/com/tiffanytimbric/reactor/Main.java
+++ src/main/java/com/tiffanytimbric/reactor/Main.java
@@ -2,18 +2,45 @@ package com.tiffanytimbric.reactor;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNull;
 import reactor.util.annotation.Nullable;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 
+/**
+ * The Reactive eXtensions API supports two methods of data flow, push and pull.
+ * The Java Streams API support supports only one method of data flow, pull.
+ * <h3>Data Flow Types</h3>
+ * <ul>
+ *     <li>Push - New data arrives asynchronously until the publisher signals its end.
+ *                The subscriber process the incoming data as it arrives.  This flow
+ *                type is alloigous to event stream, event processing.</li>
+ *     <li>Pull - A fixed block of data pre-exists before the processing of it begins.
+ *                This flow type is analigous to Java Collections.</li>
+ * </ul>
+ */
 public class Main {
 
     public static void main( @Nullable final String... args ) {
-        System.out.println("Program Start");
+        System.out.println("Program Start\n");
 
+        System.out.println("Pulling data...");
+        List<Integer> allNumbers = pullTheData();
+        System.out.println( "All Numbers: " + allNumbers );
+
+        System.out.println("\nPushing data...");
+        allNumbers = pushTheData();
+        System.out.println( "All Numbers: " + allNumbers );
+
+        System.out.println("\nProgram End");
+    }
+
+    @NonNull
+    private static List<Integer> pullTheData() {
         final Mono<Integer> zeroMono = Mono.just( 0 );
         final Flux<Integer> oddNumbersFlux = Flux.just( 1, 3, 5, 7 );
         final Flux<Integer> evenNumbersFlux = Flux.just( 2, 4, 6, 8 );
@@ -25,9 +52,22 @@ public class Main {
             .collectList()
             .blockOptional( Duration.ofSeconds( 1 ) );
 
-        System.out.println( "All Numbers: " + allNumbersOpt.get() );
+        if ( allNumbersOpt.isEmpty() ) {
+            System.err.println("Error: No numbers returned.");
 
-        System.out.println("Program End");
+            System.exit( 1 );
+        }
+
+        return allNumbersOpt.get();
+    }
+
+    @NonNull
+    private static List<Integer> pushTheData() {
+//        createEmitter( item -> System.out.println( item ), 1000l, 0 );
+//        createEmitter( consumer, 1000l, 1, 3, 5, 7 );
+//        createEmitter( consumer, 1000l, 2, 4, 6, 8 );
+
+        return new ArrayList<>();
     }
 
 }
